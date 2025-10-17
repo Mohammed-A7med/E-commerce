e3b4cab2e4647c4437310ac5a1ba143f822cc1a5
Mohammed-A7med <mohamedahmedkhalaf68@gmail.com>
2025-10-17 17:13:58 +0300

    feat(Title): create shared Title component with optional subtitle and section divider

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,6 +1,11 @@
+import Title from "../../shared/Ui/Title";
 
 export default function Home() {
   return (
+    <>
+    
     <div>Home</div>
+    <Title title="TRENDING" subTitle="Top view in this week"/>
+    </>
   )
 }
---
+++ src/shared/Ui/SectionDivider.tsx
@@ -0,0 +1,5 @@
+export default function SectionDivider() {
+  return (
+    <span className="w-8 sm:w-12 h-[1px] sm:h-[2px] bg-gray-700 inline-block mx-2"></span>
+  );
+}
